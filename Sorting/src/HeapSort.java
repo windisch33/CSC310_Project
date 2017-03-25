ca4ee9e4dd6896bea03c106319663415ce47c3d9
Robert Windisch <windisch33@gmail.com>
2017-03-24 22:27:09 -0400

    started heap sort

--- Sorting/src/HeapSort.java
+++ Sorting/src/HeapSort.java
@@ -9,7 +9,20 @@ public class HeapSort {
 	private static int[] array;
 	
 	public static void sort(int[] array) {
+		if(array.length > 1)
+		{
+			createHeap(array, array.length-1);
+		}
 		
+	}
+	
+	public static void removeLargest(int[] array) {
+		
+		
+	}
+	
+	public static void createHeap(int[] array, int end)
+	{
 		
 	}
 
