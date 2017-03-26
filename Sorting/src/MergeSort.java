90bad16e53b2235a35daf106b562399ae191d09a
Robert Windisch <windisch33@gmail.com>
2017-03-26 13:45:50 -0400

    added all test and cleaned up merge sort

--- Sorting/src/MergeSort.java
+++ Sorting/src/MergeSort.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+
 
 /**
  * A class that implements MergeSort
@@ -6,7 +6,7 @@ import java.util.Arrays;
  *
  */
 public class MergeSort {
-	
+
 	private static int[] array;
 
 	/**
@@ -27,10 +27,10 @@ public class MergeSort {
 	 * @param arrayA the array to be split
 	 */
 	private static void mergeSort(int [] arrayA) {
-		
+
 		int [] arrayB;
 		int [] arrayC;
-		
+
 		arrayB = new int[(arrayA.length/2)];
 		if(arrayA.length%2 == 0)
 		{
@@ -39,33 +39,26 @@ public class MergeSort {
 		{
 			arrayC = new int[(arrayA.length/2)+1];
 		}
-		
+
 		if(arrayA.length > 1)
 		{
 			for(int i = 0; i <= ((arrayA.length/2)-1); i++)
 			{
 				arrayB[i] = arrayA[i];			
 			}
-			
+
 			int cIndex = 0;
 			for(int i = arrayA.length/2; i <= arrayA.length-1; i++)
 			{
 				arrayC[cIndex] = arrayA[i];	
 				cIndex++;
 			}
-			
-			System.out.println(Arrays.toString(arrayA));
-			System.out.println(Arrays.toString(arrayB));
-			System.out.println(Arrays.toString(arrayC) + "\n");
-			
+
 			mergeSort(arrayB);
 			mergeSort(arrayC);
-			
+
 			merge(arrayB,arrayC,arrayA);
 		}
-
-		
-		
 	}
 
 	/**
@@ -76,11 +69,11 @@ public class MergeSort {
 	 * @param arrayA the target array
 	 */
 	private static void merge(int[] arrayB, int[] arrayC, int[] arrayA) {
-		
+
 		int indexA = 0;
 		int indexB = 0;
 		int indexC = 0;
-		
+
 		while(indexB <= arrayB.length-1 && indexC <= arrayC.length-1)
 		{
 			if(arrayB[indexB] <= arrayC[indexC])
@@ -92,10 +85,11 @@ public class MergeSort {
 				arrayA[indexA] = arrayC[indexC];
 				++indexC;
 			}
-			
+
 			++indexA;
 		}
-		
+
+		//add array not completed to end of array a
 		if(indexB == arrayB.length)
 		{
 			for(int i = indexC; i <= arrayC.length-1; i++)
@@ -110,12 +104,6 @@ public class MergeSort {
 				arrayA[indexA] = arrayB[i];
 				indexA++;
 			}
-		}
-		
-		System.out.println("Merged ArrayA: "+ Arrays.toString(arrayA)+"\n" );
-		
+		}	
 	}
-	
-	
-
 }
---
+++ Sorting/tests/AllTests.java
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Robert Windisch
+ *
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+{
+		HeapSortTest.class,
+		MergeSortTest.class,})
+
+public class AllTests {
+	
+}
