6103373b54029aa3307d6232c957cdf0693e73c7
Robert Windisch <windisch33@gmail.com>
2017-03-26 13:04:05 -0400

    merge sorting started, not working yet

--- Sorting/src/MergeSort.java
+++ Sorting/src/MergeSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 /**
  * A class that implements MergeSort
@@ -5,10 +6,110 @@
  *
  */
 public class MergeSort {
+	
+	private static int[] array;
 
-	public static void sort(int[] testArray) {
-		// TODO Auto-generated method stub
+	/**
+	 * @param inputArray
+	 */
+	public static void sort(int[] inputArray) 
+	{		
+		array = inputArray;
+		mergeSort(array);
+	}
+
+	/**
+	 * 
+	 */
+	private static void mergeSort(int [] arrayA) {
+		
+		int [] arrayB;
+		int [] arrayC;
+		
+		arrayB = new int[(arrayA.length/2)];
+		if(arrayA.length%2 == 0)
+		{
+			arrayC = new int[(arrayA.length/2)];
+		}else
+		{
+			arrayC = new int[(arrayA.length/2)+1];
+		}
+		
+		if(arrayA.length > 1)
+		{
+			for(int i = 0; i <= ((arrayA.length/2)-1); i++)
+			{
+				arrayB[i] = arrayA[i];			
+			}
+			
+			int cIndex = 0;
+			for(int i = arrayA.length/2; i <= arrayA.length-1; i++)
+			{
+				arrayC[cIndex] = arrayA[i];	
+				cIndex++;
+			}
+			
+			System.out.println(Arrays.toString(arrayA));
+			System.out.println(Arrays.toString(arrayB));
+			System.out.println(Arrays.toString(arrayC) + "\n");
+			
+			mergeSort(arrayB);
+			mergeSort(arrayC);
+			
+			merge(arrayB,arrayC,arrayA);
+		}
+
+		
 		
 	}
+
+	/**
+	 * 
+	 * @param arrayB
+	 * @param arrayC
+	 * @param arrayA
+	 */
+	private static void merge(int[] arrayB, int[] arrayC, int[] arrayA) {
+		
+		int indexA = 0;
+		int indexB = 0;
+		int indexC = 0;
+		
+		while(indexB <= arrayB.length-1 && indexC <= arrayC.length-1)
+		{
+			if(arrayB[indexB] <= arrayC[indexC])
+			{
+				arrayA[indexA] = arrayB[indexB];
+				++indexB;
+			}else
+			{
+				arrayA[indexA] = arrayC[indexC];
+				++indexC;
+			}
+			
+			++indexA;
+		}
+		
+		if(indexB == arrayB.length-1)
+		{
+			for(int i = indexC; i < arrayC.length-1; i++)
+			{
+				arrayA[indexA] = arrayC[indexC];
+				indexA++;
+			}
+		}else
+		{
+			for(int i = indexB; i < arrayB.length-1; i++)
+			{
+				arrayA[indexA] = arrayB[indexB];
+				indexA++;
+			}
+		}
+		
+		System.out.println("Merged ArrayA: "+ Arrays.toString(arrayA)+"\n" );
+		
+	}
+	
+	
 
 }
--- Sorting/tests/MergeSortTest.java
+++ Sorting/tests/MergeSortTest.java
@@ -16,12 +16,25 @@ public class MergeSortTest {
 	 */
 	@Test
 	public void testSort() {
-		int[] testArray = { 52, 3, 6, 7, 1, 95, 16 };
-		int[] sortedArray = { 1, 3, 6, 7, 16, 52, 95 };
+		
+		int[] testArray1 = { 1, 3, 2, 4, 6, 5 };
+		int[] sortedArray1 = { 1, 2, 3, 4, 5, 6 };
 
-		MergeSort.sort(testArray);
-
-		assertEquals(Arrays.toString(sortedArray), Arrays.toString(testArray));
+		MergeSort.sort(testArray1);
+		assertEquals(Arrays.toString(sortedArray1), Arrays.toString(testArray1));
+		
+//		int[] testArray1 = { 52, 3, 6, 7, 1, 95, 16 };
+//		int[] sortedArray1 = { 1, 3, 6, 7, 16, 52, 95 };
+//		
+//		int[] testArray2 = { 22, 3, 857, 29, 26, 758, 322, 1561, 9999, 3 };
+//		int[] sortedArray2 = { 3, 3, 22, 26, 29, 322, 758, 857, 1561, 9999};
+//
+//		MergeSort.sort(testArray1);
+//		MergeSort.sort(testArray2);
+//
+//
+//		assertEquals(Arrays.toString(sortedArray1), Arrays.toString(testArray1));
+//		assertEquals(Arrays.toString(sortedArray2), Arrays.toString(testArray2));
 	}
 
 }
