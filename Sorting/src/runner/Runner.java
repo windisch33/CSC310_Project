366110b802950a3768d001d92a3069e56c0358ff
Robert Windisch <windisch33@gmail.com>
2017-03-27 22:20:15 -0400

    made runner work on small files

--- Sorting/src/runner/Runner.java
+++ Sorting/src/runner/Runner.java
@@ -2,6 +2,8 @@ package runner;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -16,28 +18,105 @@ import sortingAlogrithms.QuickSort;
 public class Runner {
 
 
-	static String file = "testFile.txt";
+	static String file;
 
 	private final static int small = 10000;
 	private final static int medium = 100000;
 	private final static int large = 1000000;
+	
+	private static PrintWriter writer = null;
 
-	static int [] fileArray = new int[10];
+	static int [] fileArray;
+	
+	private static long startTime;
+	private static long totalTime;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args){
+		
+		try {
+			writer = new PrintWriter("data_results.txt", "UTF-8");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    writer.println("File Name, HeapSort, MergeSort, QuickSort, ");
 
+		smallFile();
+		writer.close();
+	}
+	
+	public static void smallFile()
+	{
+		fileArray = new int[small];
+		
+		for(int i = 1; i <= 30; i++)
+		{
+			file = "Small_Unsorted_" + i + ".txt";
+			writer.print(file + ", ");
+			
+			heapRunner(file);
+			mergeRunner(file);
+			quickRunner(file);
+			writer.print("\n");
+		}
+		
+		for(int i = 1; i <= 30; i++)
+		{
+			file = "Small_Sorted_" + i + ".txt";
+			writer.print(file + ", ");
+			
+			heapRunner(file);
+			mergeRunner(file);
+			quickRunner(file);
+			writer.print("\n");
+		}
+		
+		for(int i = 1; i <= 30; i++)
+		{
+			file = "Small_Reverse_Sort_" + i + ".txt";
+			writer.print(file + ", ");
+			
+			heapRunner(file);
+			mergeRunner(file);
+			quickRunner(file);
+			writer.print("\n");
+		}
+	}
+
+	private static void quickRunner(String file2) {
+		
 		fileToArray(fileArray, file);
-		System.out.println(Arrays.toString(fileArray));
+		startTime = System.nanoTime();
+		QuickSort.sort(fileArray);
+		totalTime = (System.nanoTime() - startTime);
+		writer.print(totalTime + ", ");
+		
+	}
 
-		long startTime = System.nanoTime();
+	private static void mergeRunner(String file2) {
+		
+		fileToArray(fileArray, file2);
+		startTime = System.nanoTime();
+		MergeSort.sort(fileArray);
+		totalTime = (System.nanoTime() - startTime);
+		writer.print(totalTime + ", ");
+		
+	}
+
+	private static void heapRunner(String file2) {
+		
+		fileToArray(fileArray, file2);
+		startTime = System.nanoTime();
 		HeapSort.sort(fileArray);
-		long endTime = System.nanoTime();
-		long totalTime = (endTime - startTime);
-
-		System.out.println(Arrays.toString(fileArray));
-		System.out.println("Took: " + totalTime + "ns");
+		totalTime = (System.nanoTime() - startTime);
+		writer.print(totalTime + ", ");
+		
 	}
 
 	/**
--- Sorting/src/sortingAlogrithms/QuickSort.java
+++ Sorting/src/sortingAlogrithms/QuickSort.java
@@ -2,4 +2,9 @@ package sortingAlogrithms;
 
 public class QuickSort {
 
+	public static void sort(int[] fileArray) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
